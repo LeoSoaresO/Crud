5f0f0b92ee5833c82e270e88a024638d39b23ed3
Leonardo Soares <leonardo.rock22@hotmail.com>
2020-03-31 17:47:35 -0300

    wip

--- index.php
+++ index.php
@@ -8,6 +8,25 @@
 </head>
 <body>
     <div class="container">
-    </div>
+    <a class="links" id="cadastro"></a>
+        <div class="content">
+           <!--HOME-->
+            <div id="home" style=" padding-bottom: 482px;">
+            <form method="post" action="">
+            <H1 style="color: #ddd">Bem Vindo</H1>
+            </form>
+            </div>
+        </div>
+    <Ul>
+        <div class="navbar">
+        <li>
+          <a href="#">Link</a>
+          <a href="#">Link</a>
+          <a href="#">Link</a>
+          <a href="#" class="right">Link</a>
+        </div>
+        </li>
+    </Ul>
+    </div>    
 </body>
 </html>
